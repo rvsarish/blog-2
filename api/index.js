a107cae3835370f9db7cd71a65cc3f98acd2c42a
rvsarish <21it210@stjosephs.ac.in>
2024-02-18 11:43:35 +0530

    user model - sucess

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
+app.use(express.json());
+mongoose.connect(process.env.MONGO).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err));
+
 app.listen(5000, () => console.log('Server Running'));
