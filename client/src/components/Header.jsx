daca02d616a13931820238d2c5775e833d69c93d
rvsarish <21it210@stjosephs.ac.in>
2024-02-18 10:47:05 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
 <div className='flex gap-2 md:order-2'>
       <Button className="w-12 h-10 sm:inline" color="gray" pill> <FaMoon /></Button>
       <Link to="/sign-in">
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
