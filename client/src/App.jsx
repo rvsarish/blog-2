b1f9804038e0da4a10d4c7f7d98f911f853c0d35
rvsarish <21it210@stjosephs.ac.in>
2024-02-18 09:21:10 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div className='text-3xl text-re'>App</div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
